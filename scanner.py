896680f0187a2cfec6d3881b44083eb4f0318e97
Igor Schultz <igorschul@gmail.com>
2023-10-04 11:04:24 -0300

    updated conformity regions

--- scanner.py
+++ scanner.py
@@ -10,9 +10,14 @@ logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 OUTPUT_FILE = "findings.json"
 
 CC_REGIONS = [
-    "eu-west-1",
-    "ap-southeast-2",
-    "us-west-2",
+    "us-1",
+    "in-1",
+    "gb-1",
+    "jp-1",
+    "de-1",
+    "au-1",
+    "ca-1",
+    "sg-1"
 ]
 
 RISK_LEVEL_NUMS = {
@@ -82,7 +87,7 @@ class CcValidator:
         return payload
 
     def run_validation(self, payload):
-        cfn_scan_endpoint = f"https://conformity.us-1.cloudone.trendmicro.com/api/template-scanner/scan"
+        cfn_scan_endpoint = f"https://conformity.{self.cc_region}.cloudone.trendmicro.com/api/template-scanner/scan"
 
         json_output = json.dumps(payload, indent=4, sort_keys=True)
         logging.debug(f"Sending the following request:\n{json_output}")
